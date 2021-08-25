00774e12ec0017a5069dd18a0b4a86b7ec0ee115
Igor <talen9812@gmail.com>
2021-08-25 17:59:18 +0700

    Удален модуль read_parameters (перенесен в другой модуль files)

--- read_parameters.py
+++
@@ -1,9 +0,0 @@
-def Read_parameters(filename):
-    parameters = []
-    with open(filename, 'rt') as f:
-        for line in f:
-            if line.startswith('#'):
-                continue
-            parameters.append(line)   
-    parameters = [x.rstrip("\n") for x in parameters]                          # удалить '/n' символы из элементов массива
-    return parameters
